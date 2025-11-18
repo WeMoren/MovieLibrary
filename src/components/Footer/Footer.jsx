5c7fd0ec988162c28d53e7842cdb1b24090eab61
uwem4life <wemoren@gmail.com>
2025-11-18 17:10:46 +0100

    added SEO

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Footer.css";
+import logo from "../../assets/WeMoren-badge.png";
 const Footer = () => {
   const year = new Date().getFullYear();
   const dataUrl = "https://www.themoviedb.org/";
   const githubUrl = "https://www.github.com/WeMoren";
+  const altText = "WeMoren logo";
   return (
     <footer className="footer">
+      <img className="footer-logo" src={logo} alt={altText} />
       <p>© {year} MovieLibrary. All rights reserved.</p>
       <p>
         Developed by{" "}
